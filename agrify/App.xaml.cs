c88f36bc116b240fff131bef46875feba8ceb110
JM <jmdgayola@gmail.com>
2025-11-04 21:51:49 +0800

    i want to kill myself

---
+++ agrify/App.xaml.cs
@@ -0,0 +1,45 @@
+using agrify;
+using Microsoft.Extensions.Logging;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Navigation;
+
+namespace agrify;
+
+public sealed partial class App : Application
+{
+    public App()
+    {
+        this.InitializeComponent();
+    }
+
+    protected Window? MainWindow { get; private set; }
+
+    protected override void OnLaunched(LaunchActivatedEventArgs args)
+    {
+        MainWindow = new Window();
+
+#if DEBUG
+        MainWindow.UseStudio();
+#endif
+
+        if (MainWindow.Content is not Frame rootFrame)
+        {
+            rootFrame = new Frame();
+            MainWindow.Content = rootFrame;
+            rootFrame.NavigationFailed += OnNavigationFailed;
+        }
+
+        if (rootFrame.Content == null)
+        {
+            rootFrame.Navigate(typeof(MainPage), args.Arguments);
+        }
+
+        MainWindow.Activate();
+    }
+
+    private void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+    {
+        throw new InvalidOperationException($"Failed to load {e.SourcePageType.FullName}: {e.Exception}");
+    }
+}
